cefa4d278ac4f3e7a6f4359bab4c6b4097208b8a
Anthony Li <li.anthony917@gmail.com>
2020-04-27 20:37:26 -0400

    Add some Kotlin

--- src/main/kotlin/ItempoolClient.kt
+++ src/main/kotlin/ItempoolClient.kt
@@ -1,5 +1,15 @@
 package dev.anli.entityocean
 
-class ItempoolClient {
-    
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.toDeferred
+
+class ItempoolClient(val url: String) {
+    val client: ApolloClient = ApolloClient.builder()
+        .useHttpGetMethodForQueries(false)
+        .serverUrl(url)
+        .build()
+
+    suspend fun getCurrentChallenge(liveId: String) {
+        client.query(LiveChallengeQuery(liveId)).toDeferred().await()
+    }
 }
